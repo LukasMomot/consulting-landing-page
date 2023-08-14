c44b3828c1295b0f64f0cd569a4d4923ebda5619
Lukasz Momot <lukasz.momot@hotmail.com>
2023-08-14 14:30:19 +0200

    fix import of fontsource

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "tailwindcss/dist/base.css";
 import "styles/globalStyles.css";
-// import "@fontsource/inter" 
+import "@fontsource/inter/latin.css";
 import React from "react";
 import { css } from "styled-components/macro"; //eslint-disable-line
 
