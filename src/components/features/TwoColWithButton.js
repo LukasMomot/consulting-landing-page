6ee1708540dc950d4815a89875f6e8e53ba5864b
Lukasz Momot <lukasz.momot@hotmail.com>
2023-08-14 14:48:19 +0200

    Release: Change col text description contact

--- src/components/features/TwoColWithButton.js
+++ src/components/features/TwoColWithButton.js
@@ -34,7 +34,7 @@ const Subheading = tw(SubheadingBase)`text-center md:text-left`;
 const Heading = tw(
   SectionHeading
 )`mt-4 font-black text-left text-3xl sm:text-4xl lg:text-5xl text-center md:text-left leading-tight`;
-const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100`;
+const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base font-medium leading-relaxed text-secondary-100`;
 
 const PrimaryButton = styled(PrimaryButtonBase)(props => [
   tw`mt-8 w-44 md:w-40 md:mt-8 text-sm inline-block mx-auto md:mx-0 text-center`,
